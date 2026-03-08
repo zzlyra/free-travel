58b817b2764ce81508d8d76ec0cfb5c28b683572
zzlyra <gongqiaoyuelyra@gmail.com>
2026-03-08 21:57:53 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+export const AI_RECOMMENDED_SITES = {
+  "Kyoto": [
+    { name: "清水寺", lat: 34.9949, lng: 135.7850, preferredTime: "morning" },
+    { name: "伏见稻荷", lat: 34.9671, lng: 135.7727, preferredTime: "morning" },
+    { name: "祗园", lat: 35.0032, lng: 135.7788, preferredTime: "afternoon" },
+    { name: "岚山", lat: 35.0094, lng: 135.6671, preferredTime: "afternoon" }
+  ]
+};
